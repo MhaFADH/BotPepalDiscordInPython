b755abbe5dcfc2e131966daa6c79821f79e6e1c2
MhaFADH <98533274+MhaFADH@users.noreply.github.com>
2022-12-14 15:57:46 +0100

    Update Bot discord.py

--- Bot discord.py
+++ Bot discord.py
@@ -1,63 +1,70 @@
-from asyncio import tasks
 import requests
 from bs4 import BeautifulSoup
 import discord
-from discord.ext import tasks, commands
+from discord.ext import tasks,commands
 
-bot = discord.Client()
+intents = discord.Intents.all()
+intents.members = True
 
-# test*
-
-def cmb_note():
-    identifiants = {'login':'','pass':''}
-
-    login_url= 'ident page'
-
-    session = requests.Session()
-
-    login = session.post(login_url, data = identifiants)
-
-    url = 'grade page'
-
-    result = session.get(url)
-
-    doc = BeautifulSoup(result.text, "html.parser")
-    tab_note = doc.find_all(class_ = "note_devoir")
-    tds = tab_note[0].find_all("td")
-
-    # print(tds[3].text)
-
-    nb_notes = 0
-
-    for i in tab_note:
-        if tds[3].text != " ":
-            nb_notes += 1
-
-    return nb_notes
+bot = commands.Bot(command_prefix=".", intents=intents)
 
 
-notes_act = cmb_note()
+class PepalToolbox:
 
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
 
-msg = "@everyone NOUVELLE NOTE DISPONIBLE SUR **** "
+    def pepal_connect(self):
+        idents = {'login' :self.username ,'pass' :self.password}
+        login_url = 'url to login page'
+        get_session = requests.Session()
+        logToPepal = get_session.post(login_url, data = idents)
+
+        return get_session
+
+    def how_many_grades(self):
+        grades_url = 'url to grades page'
+        session = self.pepal_connect()
+        page = session.get(grades_url)
+        parsedPage = BeautifulSoup(page.text, "html.parser")
+        grades_class = parsedPage.find_all(class_="note_devoir")
+        page_td = grades_class[0].find_all("td")
+
+        grades_counter = 0
+
+        for classes in grades_class:
+            if page_td[3].text != " ":
+                grades_counter += 1
+
+        return grades_counter
+
+#######################SETTINGS######################
+tools = PepalToolbox('Username','Password')
+msg = "@everyone NOUVELLE NOTE DISPONIBLE SUR PEPAL "
+old_counter = tools.how_many_grades()
+#####################################################
 
 @bot.event
 async def on_ready():
-    print("ready to go")
-    checkfornewnotes.start()
+    global tools
+    print("Bot is ready !")
+    check_for_new_grades.start()
 
+@tasks.loop(minutes=5)
+async def check_for_new_grades():
+    global old_counter
+    global tools
+    global msg
 
-@tasks.loop(minutes=30)
-async def checkfornewnotes():
-    global notes_act
-    if notes_act != cmb_note():
-        print("oui")
-        notes_act += 1
-        await bot.get_channel().send(msg)
+    new_counter = tools.how_many_grades()
+    if old_counter != new_counter:
+        print("Nouvelle note !")
+        old_counter = new_counter
+        await bot.get_channel('Channel token').send(msg)
     else:
-        print("non")
+        print("Aucune nouvelle note !")
 
 
-        
 
-bot.run('bot token')
+bot.run('Bot token')
